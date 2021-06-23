df20e1395de1ca320c648d8df7607367730c5074
akk26 <anujbph@gmail.com>
2021-06-24 00:46:24 +0530

    After rectification

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -20,28 +20,32 @@ numbers.lastIndexOf(9);
 
 // - Convert value of strings array into a sentance like "This is a collection of words"
 
-strings.join().replaceAll(",", " ");
+strings.join(" ");
 
 
 // - Add two new words in the strings array "called" and "sentance"
 
-let updatedString = strings.concat("called", "sentance");
+strings.push("called");
+strings.push("sentance");
 
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
 
-updatedString.join().replaceAll(",", " ");
+strings.join(" ")
 
 // - Remove the first word in the array (strings)
 
-updatedString.splice(0, 1);
+strings.unshift();
 
 // - Find all the words that contain 'is' use string method 'includes'
 
-updatedString.includes("is");
+let newArr = strings.filter((string) =>
+  string.includes("is")
+);
 
 // - Find all the words that contain 'is' use string method 'indexOf'
 
-updatedString.indexOf("is");
+let newArrAgain = strings.filter((string) => string.indexOf("is") !== -1
+);
 
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
 
@@ -64,9 +68,11 @@ updatedString.pop();
 
 // - Find largest number in numbers
 
-
+let largest = newArray.sort((a, b) => a - b).pop();
 
 // - Find longest string in strings
+
+let longestString = [...strings].sort((a, b) => a.length - b.length).pop();
 
 // - Find all the even numbers
 
@@ -86,23 +92,42 @@ numbers.filter(isOdd);
 
 // - Place a new word at the start of the array use (unshift)
 
-strings.unshift("New");
+[...strings].unshift("New Word!");
 
 
 // - Make a subset of numbers array [18,9,7,11]
 
-let subsetNum = [18, 9, 7, 11];
+console.log(numbers.slice(3, 7));
+
 
 // - Make a subset of strings array ['a','collection']
 
-let subsetStr = ['a', 'collection'];
+let subStr = strings.slice(3,5);
 
 // - Replace 12 & 18 with 1221 and 1881
 
+console.log(
+  numbers.map((num) => {
+    if (num === 12) {
+      return 1221;
+    } else if (num === 18) {
+      return 1881;
+    } else {
+      return num;
+    }
+  })
+);
 
 // - Replace words in strings array with the length of the word
 
+let strLength = strings.map((string) => string.length);
+
 // - Find the sum of the length of words using above question
+
+strLength.reduce((acc, cv) => {
+  acc = acc + cv;
+  return acc;
+}, 0)
 
 // - Customers Array
 var customers = [
@@ -113,18 +138,35 @@ var customers = [
 ];
 // - Find all customers whose firstname starts with 'J'
 
+let filteredCustomer = customers.filter((customer) => customer.firstname.startsWith("J")
+);
+
 // - Create new array with only first name
 
-let firstName = ['Joe', 'John', 'Dave', 'Jack'];
+let firstName = customers.map((customer) => customer.firstname
+);
 
 // - Create new array with all the full names (ex: "Joe Blogs")
 
-let fullName = ["Joe Blogs", "John Smith", "Dave Jones", "Jack White"];
-
+let fullName = customers.map((customer) => `${ customer.firstname } ${customer.lastname}`);
 
 // - Sort the array created above alphabetically;
 
-fullName.sort();
+[...fullName].sort();
 
 // - Create a new array that contains only user who has at least one vowel in the firstname.
+
+let vowelCustomer = customers.filter((customer) => {
+  if (
+    customer.firstname.toLowerCase().includes("a") ||
+    customer.firstname.toLowerCase().includes("e") ||
+    customer.firstname.toLowerCase().includes("i") ||
+    customer.firstname.toLowerCase().includes("o") ||
+    customer.firstname.toLowerCase().includes("u")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+});
 
